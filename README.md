acab60814e3f23b290c8bdd6ede4b7ead2895d8d
BetaCatPro <zj20162325@163.com>
2023-05-13 16:30:28 +0800

    feat: 初始化工具

---
+++ README.md
@@ -0,0 +1,13 @@
+## About The Project
+前端工程化脚手架-用于项目打包及构建
+
+## Getting Started
+### Installation
+
+1. Install NPM packages
+   ```sh
+   npm install
+   ```
+
+<!-- USAGE EXAMPLES -->
+## Usage
---
+++ bin/index.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+
+program
+    .option('--first')
+    .option('-s, --separator <char>');
+
+program.parse();
+
+const options = program.opts();
+const limit = options.first ? 1 : undefined;
+console.log(program.args[0].split(options.separator, limit));
+
+// $ node split.js -s / --fits a/b/c
+// error: unknown option '--fits'
+// (Did you mean --first?)
+// $ node split.js -s / --first a/b/c
+//     [ 'a' ]
