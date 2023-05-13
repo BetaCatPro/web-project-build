928360ef11239f99d67475bb8638d066d0f3b3f1
BetaCatPro <zj20162325@163.com>
2023-05-13 16:31:01 +0800

    feat: 初始化工具

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 ## About The Project
+
 前端工程化脚手架-用于项目打包及构建
 
 ## Getting Started
+
 ### Installation
 
 1. Install NPM packages
-   ```sh
-   npm install
-   ```
+    ```sh
+    npm install
+    ```
 
 <!-- USAGE EXAMPLES -->
+
 ## Usage
--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,14 @@
 #!/usr/bin/env node
 
-const { program } = require('commander');
+const { program } = require('commander')
 
-program
-    .option('--first')
-    .option('-s, --separator <char>');
+program.option('--first').option('-s, --separator <char>')
 
-program.parse();
+program.parse()
 
-const options = program.opts();
-const limit = options.first ? 1 : undefined;
-console.log(program.args[0].split(options.separator, limit));
+const options = program.opts()
+const limit = options.first ? 1 : undefined
+console.log(program.args[0].split(options.separator, limit))
 
 // $ node split.js -s / --fits a/b/c
 // error: unknown option '--fits'
